ca07630d1129b6e438a7994c5c5f6a61026dee5f
Smupyknight <smupyknight@outlook.com>
2017-08-11 02:03:02 +0800

    Initial commit with ExpressJS + Angular4 integration

---
+++ src/index.js
@@ -0,0 +1,16 @@
+var express  = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(express.static(path.join(__dirname + '/../dist')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.get('/', function(req, res, next) {
+	res.sendFile(path.join(__dirname + '/../dist/index.html'));
+});
+
+app.listen(process.env.PORT || 3000);
