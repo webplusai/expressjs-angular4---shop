5da22ec93097a9a1fea2dc447a65e8cfc2b1d931
Smupyknight <smupyknight@outlook.com>
2017-08-12 23:02:26 +0800

    Left sidebar complete

--- src/app/front/front.component.ts
+++ src/app/front/front.component.ts
@@ -2,17 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'site-content',
-	template: `
-				<md-sidenav-container>
-					<md-sidenav #leftsidenav>
-						<left-sidenav> </left-sidenav>
-					</md-sidenav>
-					<button md-icon-button id="leftSideNav" fxHide (click)="leftsidenav.open()"><md-icon md-font-set="material-icons">menu</md-icon></button>
-					<site-header></site-header>
-					<router-outlet name='front'></router-outlet>
-					<site-footer></site-footer>
-				</md-sidenav-container>
-			`,
+	templateUrl: './front.component.html',
 	host: {
 		'class': 'layout-column'
 	}
--- src/app/front/partials/sidenav/left.component.ts
+++ src/app/front/partials/sidenav/left.component.ts
@@ -4,4 +4,6 @@ import { Component } from '@angular/core';
 	selector: 'left-sidenav',
 	templateUrl: './left.component.html'
 })
-export class LeftSidenavComponent { }
+export class LeftSidenavComponent { 
+	menuItems = [ 'Accessory', 'Jewllery', 'Shoes', 'Cosmetic', 'Handmade', 'Haute Couture', 'Horology', 'Lingerle', 'Wedding', 'Perfumery', 'Ready To Wear', 'Bag', 'Wax Clothe', 'All Categories' ];
+}
