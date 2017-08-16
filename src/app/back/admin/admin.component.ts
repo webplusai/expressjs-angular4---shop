3c143544ad27875196e6c12e4e1a78192ba8f5af
Smupyknight <smupyknight@outlook.com>
2017-08-17 01:23:28 +0800

    Header content complete

--- src/app/back/admin/admin.component.ts
+++ src/app/back/admin/admin.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'admin',
-	templateUrl: './admin.component.html'
+	templateUrl: './admin.component.html',
+	styleUrls: [ './admin.component.scss' ]
 })
 export class AdminComponent { }
