83d2dfbd9d4268a8fcc88ebf48b1fba1dba45596
Smupyknight <smupyknight@outlook.com>
2017-09-30 19:14:23 +0200

    Products page shipping tab

--- src/app/back/seller/pages/products/tabs/shipping/shipping.component.ts
+++ src/app/back/seller/pages/products/tabs/shipping/shipping.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { DataSource } from '@angular/cdk/table';
+import { MdPaginator } from '@angular/material';
+import { MdSort } from '@angular/material';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/observable/fromEvent';
+
+import { TableData } from '../../../../../../helper/table-data/table-data';
 
 @Component({
-  selector: 'seller-product-tab-shipping',
-  templateUrl: './shipping.component.html',
-  styleUrls: ['./shipping.component.scss']
+	selector: 'seller-product-tab-shipping',
+	templateUrl: './shipping.component.html',
+	styleUrls: ['./shipping.component.scss']
 })
 export class ShippingComponent implements OnInit {
 
-  constructor() { }
+	shippingList = [];
+	list = [];
 
-  ngOnInit() {
-  }
+	displayedColumns = ['shipping_method', 'comment', 'cost', 'geo_zone', 'action'];
+	tableData = new TableData();
+	dataSource: ShippingDataSource | null;
 
+	constructor() { }
+
+	ngOnInit() {
+		this.tableData.setData(this.shippingList);
+		this.dataSource = new ShippingDataSource(this.tableData);
+	}
+
+	redrawTable() {
+		this.tableData.setData(this.shippingList);
+		this.dataSource = new ShippingDataSource(this.tableData);
+	}
+
+	addRow() {
+		this.shippingList.push( {destination: '', shipping_list: '', delivery_time: '', weight_from: '', weight_to: '',cost_fixed: '',  cost_per_weight: ''} );
+		this.redrawTable();
+	}
+
+	removeRow(index) {
+		this.shippingList.splice(index, 1);
+		this.redrawTable();
+	}
+}
+
+export class ShippingDataSource extends DataSource<any> {
+
+	displayDataChanges: any;
+
+	constructor(private _tableData: TableData) {
+		super();
+
+		this.displayDataChanges = [
+			_tableData.dataChange
+		]
+	}
+
+	connect(): Observable<any> {
+
+		return Observable.merge(...this.displayDataChanges).map(() => {
+			return this._tableData.data;
+		});
+	}
+
+	disconnect() {
+
+	}
 }
