f46fac3222318ba94c0dca4213d3e7ca9ad406ab
Smupyknight <smupyknight@outlook.com>
2018-02-03 00:04:28 +0800

    Remove comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } 				from 	'@angular/core';
 import { Routes, RouterModule } 	from 	'@angular/router';
 
 const routes: Routes = [
-	/*{
+	{
 		path: '',
 		loadChildren: './components/front/front.module#FrontModule'
-	},*/
+	},
 	{
 		path: 'admin',
 		loadChildren: './components/back/admin/admin.module#AdminModule'
-	}/*,
+	},
 	{
 		path: 'buyer',
 		loadChildren: './components/back/buyer/buyer.module#BuyerModule'
@@ -17,7 +17,7 @@ const routes: Routes = [
 	{
 		path: 'seller',
 		loadChildren: './components/back/seller/seller.module#SellerModule'
-	}*/
+	}
 ];
 
 @NgModule({
